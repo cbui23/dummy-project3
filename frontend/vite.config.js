c37ba3732ba78d569d3b582ad36a4d66570a214e
Garces Sam <garcesam0@tamu.edu>
2026-04-15 10:39:59 -0500

    need to edit package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 5173,
-    strictPort: true, // Prevents Vite from switching to 5174 if 5173 is busy
-    open: true,
-  },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
